528780027679f2bf0df9dba9a5d4657d13356ea0
Emad Uddin <emaduddin678@gmail.com>
2022-09-15 10:39:11 +0600

    lifecycle add

--- src/app/App.js
+++ src/app/App.js
@@ -1,53 +1,68 @@
 import React from "react";
+import axios from "axios";
 import Header from "../components/header";
 import { newsCategory } from "../news";
-import NewsList from '../components/newsList'
+import NewsList from "../components/newsList";
 import Pagination from "../components/pagination";
 import Loading from "../components/loading";
+import Lifecycle from "../lifecycle";
 
 const fakeNews = [
   {
-    title: 'Title',
-    content: 'Content',
-    url: 'https://linktonews.com',
-    urlToImage: 'https://linktoimage.com',
-    publishedAt: 'published date and time',
+    title: "Title",
+    content: "Content",
+    url: "https://linktonews.com",
+    urlToImage: "https://linktoimage.com",
+    publishedAt: "published date and time",
     source: {
-      name: 'CNN',
-    } ,
+      name: "CNN",
+    },
   },
   {
-    title: 'Title',
-    content: 'Content',
-    url: 'https://linktonews.com',
-    urlToImage: 'https://linktoimage.com',
-    publishedAt: 'published date and time',
+    title: "Title",
+    content: "Content",
+    url: "https://linktonews.com",
+    urlToImage: "https://linktoimage.com",
+    publishedAt: "published date and time",
     source: {
-      name: 'CNN',
-    } ,
-  }
-]
+      name: "CNN",
+    },
+  },
+];
 
+const URL = "https://jsonplaceholder.typicode.com/users";
+axios.get(URL).then((res) => {
+  console.log(res.data);
+});
 
+const user = {
+  name: "Emad Uddin",
+  email: "emaduddin@gmail.com",
+  username: "Emad",
+};
+axios.post(URL, user).then((res) => {
+  console.log(res);
+});
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 offset-md-3" >
-            <Header category={newsCategory.technology}/>
+          <div className="col-sm-6 offset-md-3">
+            <Header category={newsCategory.technology} />
             <div className="d-flex">
-              <p className="text-black-50">
-                About {0} results found
-              </p>
+              <p className="text-black-50">About {0} results found</p>
               <p className="text-black-50 ms-auto">
                 {1} page of {100}
               </p>
             </div>
-            <NewsList news={fakeNews}/>
+            <NewsList news={fakeNews} />
             <Pagination />
             <Loading />
+
+            {/*Todo remove this code */}
+            <Lifecycle />
           </div>
         </div>
       </div>
@@ -57,12 +72,9 @@ class App extends React.Component {
 
 export default App;
 
-
-
-// title 
-// content 
-// url 
-// urlToImage 
+// title
+// content
+// url
+// urlToImage
 // publishedAt
-// source.name 
-
+// source.name
---
+++ src/lifecycle/index.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Lifecycle extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+    console.log("Constructor Called");
+    this.divRef = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log('componentdidMount Called');
+    console.log("Mounte " ,this.divRef)
+    console.log(this.divRef.current)
+  }
+
+  componentDidUpdate() {
+    console.log("componentdidUpdate Called");
+    console.log("Update  ", this.divRef);
+    console.log(this.divRef.current);
+  }
+
+  render() {
+    console.log("Render Called");
+    console.log('Ref', this.divRef);
+    console.log(this.divRef);
+    return (
+      <div ref={this.divRef}>
+        <h3>React LifeCycle Methods</h3>
+        <p>Count: {this.state.count}</p>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Lifecycle;
